3c2ce0c7b169e7f4b941eb2e5698aa9d4be8a053
chenjia <2496243148@99.com>
2019-08-11 20:20:26 +0800

    thread-demo

---
+++ concurrency/src/main/java/com/xiaoleng/consumerProducer/EventClient.java
@@ -0,0 +1,32 @@
+package com.xiaoleng.consumerProducer;
+
+import java.util.concurrent.TimeUnit;
+
+public class EventClient {
+
+    public static void main(String[] args) {
+        final EventQueue eventQueue = new EventQueue();
+
+        new Thread(() -> {
+            while (true) {
+               eventQueue.offer(new EventQueue.Event());
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"producer").start();
+
+        new Thread(()->{
+            while (true){
+                eventQueue.take();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"consumer").start();
+    }
+}
