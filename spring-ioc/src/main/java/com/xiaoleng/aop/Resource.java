2c8d1176aa3dd19f2af480c66d37fe1b9be7bea2
chenjia <chenjia@dongfangfuli.com>
2019-08-22 12:56:41 +0800

    add classpathresource test

---
+++ spring-ioc/src/main/java/com/xiaoleng/aop/Resource.java
@@ -0,0 +1,31 @@
+package com.xiaoleng.aop;
+
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Resource {
+
+
+    public static void main(String[] args) throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("/com/xiaoleng/aop/application.properties");
+
+        File file = resource.getFile();
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        byte[] by = new byte[1024];
+        StringBuilder result = new StringBuilder();
+        while (true) {
+            int b1 = fileInputStream.read(by);
+            System.out.println(b1);
+            if (b1 == -1) {
+                break;
+            }
+            result.append(new String(by));
+        }
+        System.out.println(result.toString());
+    }
+}
--- spring-ioc/src/main/java/com/xiaoleng/aop/config/MainConfig.java
+++ spring-ioc/src/main/java/com/xiaoleng/aop/config/MainConfig.java
@@ -4,10 +4,13 @@ package com.xiaoleng.aop.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author chen jia
+ * @date 2019/08/21
+ */
 @Configuration
 @ComponentScan({"com.xiaoleng.aop"})
 public class MainConfig {
-
 
 
 }
