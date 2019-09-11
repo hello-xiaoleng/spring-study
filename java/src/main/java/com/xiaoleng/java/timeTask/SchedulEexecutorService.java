8850e1e7817a70e34ebf41f2c80ff7ef70a735d5
chenjia <chenjia@dongfangfuli.com>
2019-09-11 15:51:04 +0800

    time task

---
+++ java/src/main/java/com/xiaoleng/java/timeTask/SchedulEexecutorService.java
@@ -0,0 +1,25 @@
+package com.xiaoleng.java.timeTask;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class SchedulEexecutorService {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(5);
+
+        Runnable task1 = () -> System.out.println("timerTask1在" + LocalDateTime.now().format(DATE_TIME_FORMATTER) + ",执行了！");
+
+
+        Runnable task2 = () -> System.out.println("timerTask2在" + LocalDateTime.now().format(DATE_TIME_FORMATTER) + ",执行了！");
+        scheduledExecutorService.schedule(task1, 1, TimeUnit.SECONDS);
+
+        scheduledExecutorService.scheduleAtFixedRate(task2, 1, 1, TimeUnit.SECONDS);
+
+    }
+}
